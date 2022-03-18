383210b73e294aaed06189848059df1070de0319
Jason <safksadjf@gmail.com>
2022-03-18 08:25:08 +0100

    add in daily people vaccinated

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+	// var fixmeTop = $('table thead').offset().top;       // get initial position of the element
+	// $(window).scroll(function() {                  // assign scroll event listener
+	//     var currentScroll = $(window).scrollTop(); // get current position
+	//     if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+	//     	$('table thead').addClass('fixme');
+	//     } else {                                   // apply position: static
+	//     	$('table thead').removeClass('fixme');
+	//     }
+	// });
+});
--- js/tables.js
+++ js/tables.js
@@ -1,150 +1,155 @@
-
 function structureData() {
-	tableData = [];
-	for (let i = 0; i < vaccinations.length; i++) {
-		let x = 0;
-		while (x < populations.length) {
-			if (populations[x].countryiso3code == vaccinations[i].iso_code) {
-				var population = populations[x].value;
-				break;
-			} else {
-				var population = '';
-			}
-			x++;
-		}
-		/** Last Updated Date & Single Dose **/
+  tableData = [];
+  for (let i = 0; i < vaccinations.length; i++) {
 
-		var date_length = vaccinations[i].data.length - 1;
-		while (date_length > 0) {
-			if (vaccinations[i].data[date_length].people_vaccinated != null) {
-				var single_dose = vaccinations[i].data[date_length].people_vaccinated;
-				var last_updated_single = vaccinations[i].data[date_length].date;
-				break;
-			}
+    /** Population **/
+    let x = 0;
+    while (x < populations.length) {
+      if (populations[x].countryiso3code == vaccinations[i].iso_code) {
+        var population = populations[x].value;
+        break;
+      } else {
+        var population = "";
+      }
+      x++;
+    }
 
+    /** Daily People Vaccinated **/
+    var date_length = vaccinations[i].data.length - 1;
+    while (date_length > 0) {
+      if (vaccinations[i].data[date_length].daily_people_vaccinated != null) {
+        var daily_people_vaccinated = vaccinations[i].data[date_length].daily_people_vaccinated;
+        var last_updated_daily_people_vaccinated = vaccinations[i].data[date_length].daily_people_vaccinated;
+        break;
+      }
 
-			date_length--;
-		}
+      date_length--;
+    }
 
-		var date_length = vaccinations[i].data.length - 1;
-		while (date_length > 0) {
-			if(vaccinations[i].data[date_length].people_fully_vaccinated != null) {
-				var double_dose = vaccinations[i].data[date_length].people_fully_vaccinated;
-				var last_updated_double = vaccinations[i].data[date_length].date;
-				break;
-			}
+    /**
 
-			date_length--;
+    /** Single Dose **/
+    var date_length = vaccinations[i].data.length - 1;
+    while (date_length > 0) {
+      if (vaccinations[i].data[date_length].people_vaccinated != null) {
+        var single_dose = vaccinations[i].data[date_length].people_vaccinated;
+        var last_updated_single_dose = vaccinations[i].data[date_length].date;
+        break;
+      }
 
-		}
-		if (population == '') {
-			single_dose_percent = '';
-			double_dose_percent = '';
-		} else {
-			single_dose_percent = ((single_dose/population) * 100).toFixed(2) + '%';
-			double_dose_percent = ((double_dose/population) * 100).toFixed(2) + '%';
-		}
+      date_length--;
+    }
 
-		function addCommas(value) {
-			value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-			return value;
-		}
+    /** Double Double **/
+    var date_length = vaccinations[i].data.length - 1;
+    while (date_length > 0) {
+      if (vaccinations[i].data[date_length].people_fully_vaccinated != null) {
+        var double_dose =
+          vaccinations[i].data[date_length].people_fully_vaccinated;
+        var last_updated_double_dose = vaccinations[i].data[date_length].date;
+        break;
+      }
 
-		function changeDateFormat(value) {
-			var oldDate = value.split('-');
-			var newDate = oldDate[1] + '-' + oldDate[2] + '-' + oldDate[0];
-			return newDate;
+      date_length--;
+    }
 
-		}
-
-		population = addCommas(population);
-		single_dose = addCommas(single_dose);
-		double_dose = addCommas(double_dose);
-		last_updated_double = changeDateFormat(last_updated_double);
-		last_updated_single = changeDateFormat(last_updated_single);
-
-
-
-		tableData.push({
-			country: vaccinations[i].country,
-			population: population,
-			last_updated_single: last_updated_single,
-			single_dose: single_dose,
-			single_dose_percent: single_dose_percent,
-			last_updated_double: last_updated_double,
-			double_dose: double_dose,
-			double_dose_percent: double_dose_percent,
-
-		});
-	}	
+    /** Formatting **/
+    if (population == "") {
+      single_dose_percent = "";
+      double_dose_percent = "";
+    } else {
+      single_dose_percent = parseFloat(((single_dose / population) * 100).toFixed(2));
+      double_dose_percent = parseFloat(((double_dose / population) * 100).toFixed(2));
+    }
+    tableData.push({
+      country: vaccinations[i].country,
+      population: population,
+      daily_people_vaccinated: daily_people_vaccinated,
+      last_updated_daily_people_vaccinated: last_updated_daily_people_vaccinated,
+      last_updated_single_dose: last_updated_single_dose,
+      single_dose: single_dose,
+      single_dose_percent: single_dose_percent,
+      last_updated_double_dose: last_updated_double_dose,
+      double_dose: double_dose,
+      double_dose_percent: double_dose_percent,
+    });
+  }
 }
 
+$("th").on("click", function () {
+  let column = $(this).data("colname");
+  let order = $(this).data("order");
+  let text = $(this).html();
+  // text = text.substring(0, text.length - 1);
+  if (order == "desc") {
+    tableData = tableData.sort((a, b) => (a[column] > b[column] ? 1 : -1));
+    $(this).data("order", "asc");
+    // text += " ";
+  } else {
+    tableData = tableData.sort((a, b) => (a[column] < b[column] ? 1 : -1));
+    $(this).data("order", "desc");
+    // text += " ";
+  }
 
-$('th').on('click', function () {
-	let column = $(this).data('colname')
-	let order = $(this).data('order')
-	let text = $(this).html()
-	text = text.substring(0, text.length - 1);
-	if (order == 'desc') {
-		tableData = tableData.sort((a, b) => a[column] > b[column] ? 1 : -1);
-		$(this).data("order", "asc");
-		text += '&#9660'
-	} else {
-		tableData = tableData.sort((a, b) => a[column] < b[column] ? 1 : -1);
-		$(this).data("order", "desc");
-		text += '&#9650'
-	}
-
-	$(this).html(text)
-	buildTable(tableData);
+  $(this).html(text);
+  buildTable(tableData);
 });
 
-$('#search-input').on('keyup', function () {
-	let value = $(this).val();
-	let data = searchTable(value);
-	buildTable(data);
-})
+$("#search-input").on("keyup", function () {
+  let value = $(this).val();
+  let data = searchTable(value);
+  buildTable(data);
+});
 
 function searchTable(value) {
-	let filteredData = [];
+  let filteredData = [];
 
-	for (let i = 0; i < tableData.length; i++) {
-		value = value.toLowerCase();
-		let name = tableData[i].country.toLowerCase();
+  for (let i = 0; i < tableData.length; i++) {
+    value = value.toLowerCase();
+    let name = tableData[i].country.toLowerCase();
 
-		if (name.includes(value)) {
-			filteredData.push(tableData[i]);
-		}
-	}
-
-
-	
+    if (name.includes(value)) {
+      filteredData.push(tableData[i]);
+    }
+  }
+  return filteredData;
 }
 
 function buildTable(data) {
-	let table = document.getElementById('myTable');
+  let table = document.getElementById("myTable");
 
-	table.innerHTML = '';
-	for (let i = 0; i < data.length; i++) {
-		let row = `<tr>
-	                    <td>${tableData[i].country}</td>
-	                    <td>${tableData[i].population}</td>
+  table.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    let row = `<tr>
+                      <td>${i + 1}</td>
+	                    <td>${data[i].country}</td>
+	                    <td>${addCommas(data[i].population)}</td>
+                      <td>${addCommas(data[i].daily_people_vaccinated)}</td>
 	                    <td class="tooltip-container">
-	                		<div class="tooltip-content">
-	                			<p>Last Updated: ${tableData[i].last_updated_single}</p>
-	                			<p>At Least 1 Dose: ${tableData[i].single_dose}</p>
-	                		</div>
-	                		${tableData[i].single_dose_percent}
+    	                		<div class="tooltip-content">
+    	                			<p>Last Updated: ${changeDateFormat(data[i].last_updated_single_dose)}</p>
+    	                		</div>
+	                		   ${data[i].single_dose_percent + (data[i].single_dose_percent > 0?'%':'')}
 	                    </td>
 	                    <td class="tooltip-container">
 	                	
-	                		<div class="tooltip-content">
-	                			<p>Last Updated: ${tableData[i].last_updated_double}</p>
-	                			<p>Fully Vaccinated: ${tableData[i].double_dose}</p>
-	                		</div>
-	                		${tableData[i].double_dose_percent}
+  	                		<div class="tooltip-content">
+  	                			<p>Last Updated: ${changeDateFormat(data[i].last_updated_double_dose)}</p>
+  	                		</div>
+	                		  ${data[i].double_dose_percent + (data[i].double_dose_percent > 0?'%':'')}
 	                    </td>
-	               </tr>`
-		table.innerHTML += row;
-	}
+	               </tr>`;
+    table.innerHTML += row;
+  }
+}
+
+function addCommas(value) {
+	value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return value;
+  }
+
+  function changeDateFormat(value) {
+	var oldDate = value.split("-");
+	var newDate = oldDate[1] + "-" + oldDate[2] + "-" + oldDate[0];
+	return newDate;
 }
